23ea01b9557121f866e91d8ae69cd9bda314ed68
Boettcher, Stephen <sboettcher@pch.com>
2022-09-06 13:02:04 -0400

    add css for font weight and size

--- src/ModalInput.svelte
+++ src/ModalInput.svelte
@@ -5,7 +5,7 @@
   export let errorText;
 </script>
   
-<div class="input-wrapperr" id={`input--${name}`}>
+<div class="input-wrapper" id={`input--${name}`}>
   <label for={name}>{labelText}</label>
   <input  type="text"
           id={`${name}-id`}
@@ -20,12 +20,18 @@
   
   <style>
     input {
-        width: 25vw;
+        width: 75%;
         min-width: 350px;
         max-width: 450px;
         border: 1px solid darkgray;
         background: #fff;
         color: #000;
+        font-size: 1rem;
+        padding: 5px;
+    }
+    label {
+      font-size: 1rem;
+      font-weight: 600;
     }
   </style>
   
